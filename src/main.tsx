2c6a1a70a24ee73c2260cb803f2e3a3c11c567bf
JALLAIS Adrien <adrien.jallais@protonmail.com>
2026-03-04 14:46:35 +0100

    fix(router): adding router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
       <LanguageProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </LanguageProvider>
     </I18nextProvider>
   </StrictMode>
