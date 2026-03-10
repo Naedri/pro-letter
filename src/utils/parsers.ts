cfe58b34b42954a074424e235b6c0b31b6a50a5c
JALLAIS Adrien <adrien.jallais@protonmail.com>
2026-03-10 22:03:48 +0100

    fix: letterStyle

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -5,17 +5,18 @@
  * @example letterStyle(`She said "it's fine"`); // She said “it’s fine”
  * @example letterStyle(`"He said 'hello'"`); // “He said ‘hello’”
  * @example letterStyle(`Don't stop`); // Don’t stop
+ * @example letterStyle(`C'est l'été.`); // C'est l’été
  */
 export function letterStyle(s: string): string {
   let doubleOpen = true;
   let singleOpen = true;
 
-  return s.replace(/['"]/g, (match, index) => {
+  return s.replace(/[‘’]/g, "'").replace(/['"]/g, (match, index) => {
     const prev = s[index - 1];
     const next = s[index + 1];
 
-    // apostrophe inside word (don't, it's)
-    if (match === "'" && /\w/.test(prev) && /\w/.test(next)) {
+    // apostrophe inside word aside Unicode letter (eg. don't, l'été)
+    if (match === "'" && /\p{L}/u.test(prev) && /\p{L}/u.test(next)) {
       return "’";
     }
 
